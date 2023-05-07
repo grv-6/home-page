c850a0e426183b385dcb018f88874749e4129c09
grv-6 <you@example.com>
2023-05-07 20:51:29 +0530

    updated

--- src/component/Body.jsx
+++ src/component/Body.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Body = () => {
   return (
-    <div>
-      <div className='w-[50%] border border-blue-500'>
+    <div className='flex justify-center items-center h-[80vh]'>
+      <div className='w-[50%] border border-blue-500 '>
         <h1>Body</h1>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas laborum asperiores nesciunt sint nemo sit doloremque delectus cum odit! Quae culpa pariatur fuga dolores dignissimos asperiores accusamus? Dolore, atque quasi!</p>
       </div>
